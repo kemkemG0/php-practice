8bbde7540ad6d28e42bc7a0b0feb36f83da0d232
kemkemG0 <kenshin.jpsdeveloper@gmail.com>
2021-08-13 15:40:21 -0700

    clean master

--- post.php
+++
@@ -1,4 +0,0 @@
-<div>
-    名前:<?php echo $_POST['username']; ?>
-    パスワード:<?php echo $_POST['pwd']; ?>
-</div>
--- recive.php
+++
@@ -1,4 +0,0 @@
-<?php
-print_r($_POST["members"]);
-$account = $_POST['account'];
-echo $account['id'];
