3d22d6bf6b3ff61c5549bbc7ff2d588b6ea55306
kemkemG0 <kenshin.jpsdeveloper@gmail.com>
2021-08-15 19:04:40 -0700

    make V and C files

---
+++ app/index.php
@@ -0,0 +1,11 @@
+
+    <?php
+    require_once 'config.php';
+
+    require_once SOURCE_BASE . 'partials/header.php';
+
+    require_once SOURCE_BASE . 'controllers/register.php';
+
+    require_once SOURCE_BASE . 'partials/footer.php';
+
+    ?>
